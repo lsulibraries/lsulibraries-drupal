1dc7674108164e228e47c72b64ca5f28f0ca2d69
Jacob Sanford <jsanford@unb.ca>
2015-03-10 15:24:04 -0300

    Specify SOLR properties as overrides

--- attributes/solr.rb
+++ attributes/solr.rb
@@ -1,2 +1,2 @@
-default['unblibraries-drupal']['install-profile-slug'] = 'solr'
-default['unblibraries-drupal']['install-profile-name'] = 'unb_chef_solr'
+override['unblibraries-drupal']['install-profile-slug'] = 'solr'
+override['unblibraries-drupal']['install-profile-name'] = 'unb_chef_solr'
