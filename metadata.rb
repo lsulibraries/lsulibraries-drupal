85551417a3ef5c694fdad3e96ba34a74be2a58f0
Jacob Sanford <jsanford@unb.ca>
2015-04-21 15:32:37 -0300

    Pin dependencies

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Drupal wrapper recipe for UNB Libraries'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends "unblibraries-apache2"
-depends "database"
-depends "unblibraries-casperjs"
-depends "unblibraries-solr"
+depends 'unblibraries-apache2'
+depends 'database', '~> 2.2.0'
+depends 'unblibraries-casperjs'
+depends 'unblibraries-solr'
