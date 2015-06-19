050dd30e092a25169ab3f5510405d8354ff44e6b
Brian Cassidy <brian.cassidy@gmail.com>
2015-06-19 15:24:29 -0300

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jsanford@unb.ca'
 license          'All rights reserved'
 description      'Drupal wrapper recipe for UNB Libraries'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'unblibraries-apache2'
 depends 'database', '~> 2.2.0'
